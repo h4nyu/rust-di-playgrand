323e43afe27cbcbae276795ca10a9ff3b2c35061
xinyuan.yao <yao.ntno@gmail.com>
2020-05-29 23:58:11 +0000

    add example

--- examples/trait-generic.rs
+++ examples/trait-generic.rs
@@ -1,43 +1,53 @@
 trait A {
-    fn a(&self);
+    fn a(&self) -> String;
 }
 
 trait B {
-    fn b(&self);
+    fn b(&self) -> String;
 }
 
-struct ImplB <T>
-where T: A
+struct ImplB<T>
+where
+    T: A,
 {
-    srv_a: T
+    srv_a: T,
 }
 
-struct ImplA{}
+struct ImplA {}
 
-impl A for ImplA{
-    fn a(&self) {
-        println!("a");
+impl A for ImplA {
+    fn a(&self) -> String {
+        format!("a")
     }
 }
 
-impl <T> B for ImplB <T>
-where T: A
+impl<T> B for ImplB<T>
+where
+    T: A,
 {
-    fn b(&self) {
-        self.srv_a.a();
-        println!("b");
+    fn b(&self) -> String {
+        format!("{}, {}", self.srv_a.a(), "b")
     }
 }
 
 fn main() {
-    struct MockA;
-    impl A for MockA{
-        fn a(&self){
-            println!("mock-a");
-        }
-    }
-    let b = ImplB {
-        srv_a: MockA{}
-    };
+    let a = ImplA {};
+    let b = ImplB { srv_a: a };
     b.b();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_srv_b() {
+        struct MockA;
+        impl A for MockA {
+            fn a(&self) -> String {
+                format!("mock-a")
+            }
+        }
+        let b = ImplB { srv_a: MockA {} };
+        assert_eq!("mock-a, b", &b.b())
+    }
 }
